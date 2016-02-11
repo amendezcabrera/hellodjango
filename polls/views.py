7ccd5cc15ba7614d17ec77a8caf11f8a56326f65
Alberto <alberto@mendezcabrera.com>
2016-02-11 21:45:06 +0100

    Mostramos error 404 mediante atajo si la encuesta no existe

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
@@ -11,11 +11,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {'question':question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
