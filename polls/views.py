60715a45588d7eb5a39a6da5ccad25fa1b4e296f
Alberto <alberto@mendezcabrera.com>
2016-02-11 21:38:03 +0100

    Reformateo de código

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
@@ -19,4 +18,4 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s."%question_id)
+    return HttpResponse("You are voting on question %s." % question_id)
